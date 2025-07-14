774c4d29d492ccd83511732709ccd2d8e9741ab7
LivYank <viayank.18@gmail.com>
2025-07-14 11:35:24 +0000

    Fix: resolve merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
-=======
->>>>>>> 664e4f0ea749cdc9d4fe805b846ac7fda3596722
 import Home from "./pages/Home";
 import ASLTranslator from "./pages/ASLTranslator";
 import NotFound from "./pages/NotFound";
@@ -23,16 +19,10 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<Landing />} />    
+          <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} /> 
-          <Route path="/asl-translator" element={<ASLTranslator />} /> 
-=======
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/asl-translator" element={<ASLTranslator />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
->>>>>>> 664e4f0ea749cdc9d4fe805b846ac7fda3596722
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
